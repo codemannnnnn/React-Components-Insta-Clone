64f219728c4e0af8f0a784b03d35176e8f4cd4c3
codemannnnnn <codybranson@gmail.com>
2020-04-14 17:00:24 -0600

    new updates to this tough project

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,13 +3,19 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
 
-const PostsPage = () => {
+
+const PostsPage = ({post}) => {
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+        {
+          post.map(post => {
+          return <Post post = {post} />
+        })
+      }
     </div>
   );
 };
